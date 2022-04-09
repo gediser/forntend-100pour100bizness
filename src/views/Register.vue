cf7ae2182f77a8f2ec431abfbdb450848984f8e9
GEDISER Douala <gediserdouala2021@gmail.com>
2022-04-09 13:03:35 +0200

    loading and validation message errors for register and login

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -10,6 +10,16 @@
         </p>
       </div>
       <form class="mt-8 space-y-6" @submit="register">
+        <Alert
+          v-if="Object.keys(errors).length"
+          class="flex-col items-stretch text-sm"
+        >
+          <div v-for="(field, i) of Object.keys(errors)" :key="i">
+            <div v-for="(error, ind) of errors[field] ||  []" :key="ind">
+              * {{ error }}
+            </div>
+          </div>
+        </Alert>
         <input type="hidden" name="remember" value="true" />
         <div class="rounded-md shadow-sm -space-y-px">
           <div>
@@ -35,6 +45,10 @@
             <span class="absolute left-0 inset-y-0 flex items-center pl-3">
               <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
             </span>
+             <svg v-if="loading" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+              <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+              <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
             Sign up
           </button>
         </div>
@@ -46,6 +60,8 @@
 import { LockClosedIcon } from '@heroicons/vue/solid'
 import store from "../store"
 import {useRouter} from "vue-router"
+import Alert from "../components/Alert.vue"
+import {ref} from "vue"
 
 const router = useRouter();
 
@@ -55,15 +71,26 @@ const user = {
   password: '',
   password_confirmation: ''
 }
+let loading = ref(false);
+let errors = ref({})
 
 function register(ev){
   ev.preventDefault();
+
+  loading.value = true;
   store
     .dispatch('register', user)
     .then((res) => {
+      loading.value = false
       router.push({
         name: 'Dashboard'
       })
+    })
+    .catch((err)=>{
+      loading.value = false;
+      if (err.response.status === 422){
+        errors.value = err.response.data.errors
+      }
     })
 }
 

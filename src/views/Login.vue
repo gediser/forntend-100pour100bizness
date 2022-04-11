2ade17615197495598a01477ed34775923254295
GEDISER Douala <gediserdouala2021@gmail.com>
2022-04-11 11:28:01 +0200

    Conversion en francais des interfaces d'enregistrement et de connexion

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,12 +2,12 @@
 <template>
     <div>
       <div>
-        <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
-        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+        <img class="mx-auto h-12 w-auto" src="/images/logo.jpg" alt="Buziness" />
+        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Connectez-vous</h2>
         <p class="mt-2 text-center text-sm text-gray-600">
-          Or
+          Ou
           {{ ' ' }}
-          <router-link :to="{name: 'Register'}" class="font-medium text-indigo-600 hover:text-indigo-500"> register for free </router-link>
+          <router-link :to="{name: 'Register'}" class="font-medium text-indigo-600 hover:text-indigo-500"> Inscrivez-vous gratuitement </router-link>
         </p>
       </div>
       <form class="mt-8 space-y-6" @submit="login">
@@ -25,23 +25,23 @@
         <input type="hidden" name="remember" value="true" />
         <div class="rounded-md shadow-sm -space-y-px">
           <div>
-            <label for="email-address" class="sr-only">Email address</label>
-            <input v-model="user.email" id="email-address" name="email" type="email" autocomplete="email" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
+            <label for="email-address" class="sr-only">Adresse mail</label>
+            <input v-model="user.email" id="email-address" name="email" type="email" autocomplete="email" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Addresse mail" />
           </div>
           <div>
-            <label for="password" class="sr-only">Password</label>
-            <input v-model="user.password" id="password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
+            <label for="password" class="sr-only">Mot de passe</label>
+            <input v-model="user.password" id="password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Mot de passe" />
           </div>
         </div>
 
         <div class="flex items-center justify-between">
           <div class="flex items-center">
             <input v-model="user.remember" id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
-            <label for="remember-me" class="ml-2 block text-sm text-gray-900"> Remember me </label>
+            <label for="remember-me" class="ml-2 block text-sm text-gray-900"> Se souvenir de moi </label>
           </div>
 
           <div class="text-sm">
-            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Forgot your password? </a>
+            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Mot de passe oubli&eacute;? </a>
           </div>
         </div>
 
@@ -54,7 +54,7 @@
               <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
               <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
             </svg>
-            Sign in
+            Connexion
           </button>
         </div>
       </form>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
       <div>
-        <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
-        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Register for free</h2>
+        <img class="mx-auto h-12 w-auto" src="/images/logo.jpg" alt="Buziness" />
+        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Inscrivez-vous gratuitement</h2>
         <p class="mt-2 text-center text-sm text-gray-600">
-          Or
+          Ou
           {{ ' ' }}
-          <router-link :to="{name: 'Login'}" class="font-medium text-indigo-600 hover:text-indigo-500"> login to your account </router-link>
+          <router-link :to="{name: 'Login'}" class="font-medium text-indigo-600 hover:text-indigo-500"> Connectez-vous </router-link>
         </p>
       </div>
       <form class="mt-8 space-y-6" @submit="register">
@@ -23,20 +23,20 @@
         <input type="hidden" name="remember" value="true" />
         <div class="rounded-md shadow-sm -space-y-px">
           <div>
-            <label for="fullname" class="sr-only">Full name</label>
-            <input v-model="user.name" id="fullname" name="name" type="text" autocomplete="name" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Full name" />
+            <label for="fullname" class="sr-only">Nom complet</label>
+            <input v-model="user.name" id="fullname" name="name" type="text" autocomplete="name" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Nom complet" />
           </div>
           <div>
-            <label for="email-address" class="sr-only">Email address</label>
-            <input v-model="user.email" id="email-address" name="email" type="email" autocomplete="email" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
+            <label for="email-address" class="sr-only">Addresse mail</label>
+            <input v-model="user.email" id="email-address" name="email" type="email" autocomplete="email" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Adresse mail" />
           </div>
           <div>
-            <label for="password" class="sr-only">Password</label>
-            <input v-model="user.password" id="password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
+            <label for="password" class="sr-only">Mot de passe</label>
+            <input v-model="user.password" id="password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Mot de passe" />
           </div>
            <div>
-            <label for="password_confirmation" class="sr-only">Password Confirmation</label>
-            <input v-model="user.password_confirmation" id="password_confirmation" name="password_confirmation" type="password" autocomplete="current-password_confirmation" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password Confirmation" />
+            <label for="password_confirmation" class="sr-only">Confirmation mot de passe</label>
+            <input v-model="user.password_confirmation" id="password_confirmation" name="password_confirmation" type="password" autocomplete="current-password_confirmation" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Confirmation mot de passe" />
           </div>
         </div>
 
@@ -49,7 +49,7 @@
               <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
               <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
             </svg>
-            Sign up
+            Inscription
           </button>
         </div>
       </form>

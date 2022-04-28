e03574429033fbe89ac74b1f582e9461e018336d
GEDISER Douala <gediserdouala2021@gmail.com>
2022-04-28 13:10:21 +0200

    UserDetailsPopUp background transparent gray added

--- src/components/UserDetailsPopUp.vue
+++ src/components/UserDetailsPopUp.vue
@@ -1,12 +1,12 @@
 <template>
-    <div v-if="userDetailsPopUp.show" class="fixed top-0 left-0 right-0 z-50 w-full overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
-        <div class="relative w-full h-full max-w-2xl p-4 md:h-auto">
+    <div v-if="userDetailsPopUp.show" class="fixed top-0 left-0 right-0 z-50 w-full overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full bg-gray-500/50">
+        <div class="relative w-full h-full  p-4  flex justify-center items-center">
             <div
                 class="relative bg-white rounded-lg shadow dark:bg-gray-700 w-[300px] py-2 px-4 animate-fade-in-down border-2 border-black-600"
             >
                 <div class="flex justify-between items-center pb-2">
                         <h2 class="text-indigo-600 font-bold">Infos Publieur</h2>
-                        <svg @click="close" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <svg @click="close" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>
                 </div>
